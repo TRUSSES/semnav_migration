2c8c29bd6e0a3fa1def6a562784b23fe4e32a858
eTNwang <erictnwang@gmail.com>
2024-04-12 17:23:48 +0000

    working

--- include/reactive_planner_lib.h
+++ include/reactive_planner_lib.h
@@ -31,8 +31,14 @@
 // ROS imports
 #include "rclcpp/rclcpp.hpp"
 
+
+// to replace replacing transform datatypes/listener
 #include <tf2/transform_datatypes.h>
-#include <tf2/transform_listener.h>
+#include <tf2/convert.h>
+#include <tf2_ros/buffer.h>
+
+
+
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <message_filters/subscriber.h>
